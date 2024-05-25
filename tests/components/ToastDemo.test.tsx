7b0133b191276e6a1839a0fb3346d73b0629168b
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-05-25 12:06:25 +0300

    unit tests ToastDemo

---
+++ tests/components/ToastDemo.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Toaster } from 'react-hot-toast'
+import ToastDemo from '../../src/components/ToastDemo'
+
+describe('ToastDemo', () => {
+  it('should render correctly', async () => {
+    render(
+      <>
+        <ToastDemo />
+        <Toaster />
+      </>
+    )
+    const button = screen.getByRole('button')
+    userEvent.setup()
+    await userEvent.click(button)
+    const toast = await screen.findByText(/success/i)
+    expect(toast).toBeInTheDocument()
+  })
+})
