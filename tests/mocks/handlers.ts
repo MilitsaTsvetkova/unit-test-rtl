7d3718d9c2357d9f149a1f8c222cb590cc403efa
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-05-26 09:54:18 +0300

    unit tests ProductList

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,13 +1,18 @@
 import { http, HttpResponse } from 'msw'
 
 export const handlers = [
-  // Intercept "GET https://example.com/user" requests...
   http.get('/categories', () => {
-    // ...and respond to them using this JSON response.
     return HttpResponse.json([
       { id: 1, name: 'Electronics' },
       { id: 2, name: 'Beauty' },
       { id: 3, name: 'Gardening' },
+    ])
+  }),
+  http.get('/products', () => {
+    return HttpResponse.json([
+      { id: 1, name: 'Product1' },
+      { id: 2, name: 'Product2' },
+      { id: 3, name: 'Product3' },
     ])
   }),
 ]
