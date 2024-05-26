a1dc6189189e29db91f2e6bdf9068b756ba5fd2f
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-05-26 12:26:06 +0300

    remove mock data and use same technique to mock and get data

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,18 +1,29 @@
 import { render, screen } from '@testing-library/react'
 import { http, HttpResponse } from 'msw'
 import ProductDetail from '../../src/components/ProductDetail'
-import { products } from '../mocks/data'
+import { db } from '../mocks/db'
 import { server } from '../mocks/server'
 
 describe('ProductDetail', () => {
+  let productId: number
+  beforeAll(() => {
+    const product = db.product.create()
+    productId = product.id
+  })
+  afterAll(() => {
+    db.product.delete({ where: { id: { equals: productId } } })
+  })
   it('should render product details', async () => {
-    render(<ProductDetail productId={1} />)
+    const product = db.product.findFirst({
+      where: { id: { equals: productId } },
+    })
+    render(<ProductDetail productId={productId} />)
 
     expect(
-      await screen.findByText(new RegExp(products[0].name))
+      await screen.findByText(new RegExp(product!.name))
     ).toBeInTheDocument()
     expect(
-      await screen.findByText(new RegExp(products[0].price.toString()))
+      await screen.findByText(new RegExp(product!.price.toString()))
     ).toBeInTheDocument()
   })
   it('should render message if product not found', async () => {
--- tests/mocks/data.ts
+++
@@ -1,5 +0,0 @@
-export const products = [
-  { id: 1, name: 'Product1', price: 10 },
-  { id: 2, name: 'Product2', price: 20 },
-  { id: 3, name: 'Product3', price: 30 },
-]
