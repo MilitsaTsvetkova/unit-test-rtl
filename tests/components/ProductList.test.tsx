456621984c096d51b1d7324670154cbce404aaf2
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-05-26 13:22:33 +0300

    testing loading state ProductList

--- tests/components/ProductList.test.tsx
+++ tests/components/ProductList.test.tsx
@@ -1,5 +1,9 @@
-import { render, screen } from '@testing-library/react'
-import { http, HttpResponse } from 'msw'
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
+import { delay, http, HttpResponse } from 'msw'
 import ProductList from '../../src/components/ProductList'
 import { db } from '../mocks/db'
 import { server } from '../mocks/server'
@@ -33,5 +37,28 @@ describe('ProductList', () => {
 
     const message = await screen.findByText(/error/i)
     expect(message).toBeInTheDocument()
+  })
+  it('should render a loading indicator when fetching data', async () => {
+    server.use(
+      http.get('/products', async () => {
+        await delay()
+        return HttpResponse.json([])
+      })
+    )
+    render(<ProductList />)
+
+    const message = await screen.findByText(/loading/i)
+    expect(message).toBeInTheDocument()
+  })
+  it('should remove loading indicator after data is fetched', async () => {
+    render(<ProductList />)
+
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+  })
+  it('should remove loading indicator if data fetching fails', async () => {
+    server.use(http.get('/products', () => HttpResponse.error()))
+    render(<ProductList />)
+
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
   })
 })
