6f34bcc763cda9a73ffbc06373459b0c5f16df92
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-05-26 16:20:17 +0300

    test loading / error on BrowseProductsPage

--- tests/mocks/db.ts
+++ tests/mocks/db.ts
@@ -1,11 +1,16 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 import { faker } from '@faker-js/faker'
-import { factory, primaryKey } from '@mswjs/data'
+import { factory, manyOf, primaryKey } from '@mswjs/data'
 
 export const db = factory({
   product: {
     id: primaryKey(faker.number.int),
     name: faker.commerce.productName,
     price: () => faker.number.int({ min: 1, max: 100 }),
+  },
+  category: {
+    id: primaryKey(faker.number.int),
+    name: faker.commerce.department,
+    products: manyOf('product'),
   },
 })
---
+++ tests/pages/BrowseProductsPage.test.tsx
@@ -0,0 +1,74 @@
+import { Theme } from '@radix-ui/themes'
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
+import { delay, http, HttpResponse } from 'msw'
+import BrowseProducts from '../../src/pages/BrowseProductsPage'
+import { server } from '../mocks/server'
+
+describe('BrowseProductsPage', () => {
+  const renderComponent = () => {
+    render(
+      <Theme>
+        <BrowseProducts />
+      </Theme>
+    )
+  }
+  it('should show a loading skeleton when fetching categories ', () => {
+    server.use(
+      http.get('/categories', async () => {
+        await delay()
+        HttpResponse.json([])
+      })
+    )
+    renderComponent()
+    expect(
+      screen.getByRole('progressbar', { name: /categories/i })
+    ).toBeInTheDocument()
+  })
+  it('should hide the loading skeleton after categories are fetched', async () => {
+    renderComponent()
+    await waitForElementToBeRemoved(() =>
+      screen.queryByRole('progressbar', { name: /categories/i })
+    )
+  })
+  it('should show a loading skeleton when fetching products ', () => {
+    server.use(
+      http.get('/products', async () => {
+        await delay()
+        HttpResponse.json([])
+      })
+    )
+    renderComponent()
+    expect(
+      screen.getByRole('progressbar', { name: /products/i })
+    ).toBeInTheDocument()
+  })
+  it('should hide the loading skeleton after products are fetched', async () => {
+    renderComponent()
+    await waitForElementToBeRemoved(() =>
+      screen.queryByRole('progressbar', { name: /products/i })
+    )
+  })
+
+  it('should not render an error if categories cannot be fetched', async () => {
+    server.use(http.get('/categories', () => HttpResponse.error()))
+    renderComponent()
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByRole('progressbar', { name: /categories/i })
+    )
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument()
+    expect(
+      screen.queryByRole('combobox', { name: /category/i })
+    ).not.toBeInTheDocument()
+  })
+  it('should not render an error if products cannot be fetched', async () => {
+    server.use(http.get('/products', () => HttpResponse.error()))
+    renderComponent()
+
+    expect(await screen.findByText(/error/i)).toBeInTheDocument()
+  })
+})
